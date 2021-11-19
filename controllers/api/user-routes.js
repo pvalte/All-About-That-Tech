fc9ceae6bc793db5eae1679cd239d32220e8a730
Paige A <pvaltemare@gmail.com>
2021-11-19 14:34:13 -0800

    user can log in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,54 +42,46 @@ router.post('/', (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
-    })
-    .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });    
-    
-    // .then(dbUserData => {
-        //     req.session.save(() => {
-        //         req.session.user_id = dbUserData.id;
-        //         req.session.username = dbUserData.username;
-        //         req.session.loggedIn = true;
+    })    
+    .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
 
-        //         res.json(dbUserData);
-        //     });
-    //    })
+                res.json(dbUserData);
+            });
+       })
 });
 
-// router.post('/login', (req, res) => {
-//     User.findOne({
-//         where: {
-//             email: req.body.email
-//         }
-//     }).then(dbUserData => {
-//         if (!dbUserData) {
-//             res.status(400).json({ message: 'No user with that email address!' });
-//             return;
-//         }
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
 
-//         //res.json({ user: dbUserData });
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
 
-//         // Verify user
-//         const validPassword = dbUserData.checkPassword(req.body.password);
-//         if (!validPassword) {
-//             res.status(400).json({ message: 'Incorrect password!' });
-//             return;
-//         }
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
-//         req.session.save(() => {
-//             // declare session variables
-//             req.session.user_id = dbUserData.id;
-//             req.session.username = dbUserData.username;
-//             req.session.loggedIn = true;
-
-//             res.json({ user: dbUserData, message: 'You are now logged in!' });
-//         });
-//     });
-// });
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
 
 router.put('/:id', (req, res) => {
     User.update(req.body, {
@@ -130,15 +122,15 @@ router.delete('/:id', (req, res) => {
         });
 });
 
-// router.post('/logout' (req, res) => {
-//     if (req.session.loggedIn) {
-//         req.session.destroy(() => {
-//             res.status(204).end();
-//         });
-//     }
-//     else {
-//         res.status(404).end();
-//     }
-// });
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,7 +20,8 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('homepage', {
-                posts
+                posts,
+                loggedIn: req.session.loggedIn
             });
         })
         .catch(err => {
@@ -29,5 +30,13 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
 
 module.exports = router;
