a86c7976ebcd1a5520b13656b6612d106a0a19b1
Paige A <pvaltemare@gmail.com>
2021-11-18 22:17:43 -0800

    start styling homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('index', {
+            res.render('homepage', {
                 posts
             });
         })
