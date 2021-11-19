eb3758b6822b9e04623051dcb1d5c7bb304e3cba
Paige A <pvaltemare@gmail.com>
2021-11-18 22:03:15 -0800

    add handlebar layouts

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('index', {
+                posts
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
