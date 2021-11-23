a4659ac59c2588162de00963413e031ae22fc4a7
Paige A <pvaltemare@gmail.com>
2021-11-22 22:35:47 -0800

    idle time out and styling

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
+  cookie: {
+    expires: 5 * 60 * 1000
+  },
+  resave: true,
+  saveUninitialized: false,
   store: new SequelizeStore({
     db: sequelize
   })
