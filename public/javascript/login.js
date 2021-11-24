e994244e737f3b3ac65382dcc910c6f3d4d5fca0
Paige A <pvaltemare@gmail.com>
2021-11-23 20:02:55 -0800

    finish styling

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,30 +1,3 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        // check the response status
-        if (response.ok) {
-            console.log('signup successful');
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -50,5 +23,4 @@ async function loginFormHandler(event) {
     }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
